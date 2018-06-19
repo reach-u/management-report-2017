379d4a7bb234761124571def45acbd7b52da5cb9
Jevgeni Virves <jevgeni.virves@reach-u.com>
2018-06-19 16:08:27 +0300

    Management_Report_2017

--- src/et/Management_Report_2017.js
+++ src/et/Management_Report_2017.js
@@ -9,47 +9,42 @@
 
 <h1>Tegevusaruanne 2017</h1>
 <p>
-Reach-U on tarkvaraettevõte. Meie põhilised tegevusvaldkonnad on:
+Reach-U on oma pea 30 tegevusaasta jooksul arenenud kaartide tegijast geoinfosüsteemide loojaks ja sealt edasi tarkvaraettevõtteks. Rõhutamaks seda muutuste teed asendasime sellel aastal tegevuskaardid toimiva Javascripti koodiga (https://github.com/reach-u/management-report-2017). Tarkvarakaugete lugejate jaoks on tegevusaruanne kättesaadav ka aadressil http://www.reach-u.com/management-report-2017.html
+</p>
+<p>
+Reach-U põhilised tegevusvaldkonnad on:
 </p>
 <ul>
-  <li>a. telekomiettevõtete aitamine olemasolevatele klientidele veelgi parema teenuse pakkumisel ning uute ärisuundade leidmisel</li>
+  <li>a. telekomiettevõtete aitamine olemasolevatele klientidele parema teenuse pakkumisel ning uute ärisuundade leidmisel</li>
   <li>b. riigi- ja linnajuhtimise lihtsustamine ning läbipaistvamaks muutmine</li>
 </ul>
 <p>
-Maailmamajanduse positiivses toetuses oli 2017. aasta ettevõttele väga hea. Müügitulu kasvas üle <span class="salesgrowth">50%</span>, seda eelkõige telekomisektoris. Kasvas ka välisturgudele tehtavate lahenduste osakaal, jaotades käibe suhtega 1/3 Eesti, 1/3 muu Euroopa Liit, 1/3 muu maailm. Täname oma kliente ja partnereid usalduse eest.
+Maailmamajanduse positiivses toetuses oli aasta väga hea. Müügitulu kasvas üle 50%, kõige rohkem telekomisektoris ja väljaspool Euroopa Liitu. Piirkonniti jaotus käive: 1/3 Eesti, 1/3 muu Euroopa Liit, 1/3 muu maailm. 
 </p>
 <p>
-Reach-U meeskonnas panustas 2017. aastal 66 inimest. Täiskohale taandatud töötajate arv oli 44.
+2017. aastal oli Reach-U tootearenduse maht 400 000 EUR, ehk 12% käibest. Kõik tootearenduse ja turuhõive investeeringud kajastame perioodi kuludena.
 </p>
 <p>
-2018. aastaks plaanime sarnast finantstulemust eelmisega.
-</p>
-<p>
-2017. aastal oli Reach-U R&D tegevuse eelarve 400 000 EUR, ehk 12% ettevõtte käibest. 2018. aastal jätkame R&D tegevust kahes valdkonnas:
+2018. aastal on peamised investeeringuvaldkonnad:
 </p>
 <ul>
   <li>a. telekomisektori andmete väärtustamine ning visualiseerimine</li>
   <li>b. teede infrastruktuuri automaatkaardistamise ning 3D mudelite loomine</li>
+  <li>c. Aafrika ja Aasia turgudele sisenemine</li>
 </ul>
 <p>
-  Samuti jätkame investeeringuid Aafrika ja Aasia turgudele sisenemiseks.
-  Kõik R&D ja turuhõive investeeringud kajastame perioodi kuludena.
-</p>
-<p>
-  Kuna 37% Reach-U käibest laekub välisvaluutas, on valuutakursside kõikumine ettevõtte finantstulemust mõjutav. 2017. aastal kandsime kursimuudatustest kahju 33 981 EUR.
-</p>
-<p>
-Reach-U on oma pea 30 tegevusaasta jooksul arenenud kaartide tegijast geoinfosüsteemide loojaks ja sealt edasi tarkvaraettevõtteks. Tähistamaks seda verstaposti, oleme tegevusaruande kirjutanud koodina, mis käivitamisel annab järgmise tulemuse: managementreport2017.reach-u.com
+Kuna 37% Reach-U käibest laekus välisvaluutas, mõjutas valuutakursside kõikumine finantstulemust märgatavalt: 2017. aastal oli kahju kursimuudatustest 33 981 EUR.
 </p>
 
-<!-- KOMMENTAAR? -->
+
+<!-- Suur tänu vahva aasta eest meie headele klientidele! -->
 <p class="slidetext friends">
  <span>Kliendid ja partnerid:</span> <span> Aare(EE), Abdulwahab(KE), Aleksandr(UA), Amar(CA), Andreas(SE), Andres(EE), Andres(EE), Andres(EE), Ankit(CA), Auli(FI), Ave(EE), Baizhang(JP), Carl-Johan(SE), Chinmaya(CA), Chris(JP), Danilo(EE), Doris(EE), Eamonn(JP), Edison(JP), Eglit(EE), Eleanor(LC), Elina(EE), Eng. Keno(KE), Enn(EE), Erki(EE), Erkki(EE), Evelin(EE), Evely(EE), Felipe(CL), Fiorenzo(CA), Franciscoabel(MX), Gerttu(EE), Hamza(CA), Hannes(EE), Haseeb(SA), Husnu(TR), Imran(AE), Indrek(EE), Indrek(EE), Indrek(EE), Jaan(EE), Jaanus(EE), Jaanus(EE), Jan(CZ), Jan(CZ), Jana(EE), Janno(EE), Janno(EE), Jefferson(CA), Jeroen(SE), Jia(SE), Jihad(AE), Jindrich(CZ), Johan(BE), Joosep(EE), Josh(CA), Jürgen(EE), Juri(EE), Jüri(EE), Jussi(FI), Kaiko(EE), Kaisi(EE), Kätlin(EE), Konsta(FI), Kristjan(EE), Kristjan(EE), Kristjan(EE), Kristjan(EE), Kristjan(EE), Kristo(EE), Lauri(EE), Lauri(EE), Liia(EE), Liilia(EE), Liina(EE), Luis(MX), Maarja(EE), Maarja(EE), Maie(EE), Mailis(EE), Manke(SE), Marge(EE), Margus(EE), Marius(EE), Mark (JP), Markku(FI), Martin(EE), Martin(EE), Martina(EE), Matis(EE), Mats(EE), Matt(CA), Mattias(SE), Mehis(EE), Meshack(KE), Michael(SE), Mihkel(EE), Mihkel(EE), Mikael(FI), Mikk(EE), Mikk(EE), Momoko(JP), Moonika(EE), Mukesh(CA), Naeem(SA), Nauby(CA), Olena(CA), Omar(CA), Õnne(EE), Ove(EE), Petr(CZ), Priit(EE), Priit(EE), Priit(EE), Priit(EE), Rado(SK), Rain(EE), Rain(EE), Rando-Juku(EE), Rein(EE), Richard(CA), Robin(KE), Roland(FI), Roman(EE), Romet(EE), Rustam(UA), Sahar(CA), Svetlana(EE), Swagat(JP), Tambet(EE), Tanel(EE), Tanel(EE), Tarvo(EE), Taufiq(KE), Thomas(SE), Tom(KE), Tomas(SE), Tõnis(EE), Tony(JP), Toomas(EE), Triin(EE), Tuomo(FI), Tuuli(EE), Ugur(TR), Umer(AE), Urmas(EE), Väino(EE), Valdis(LV), Valdo(EE), Viljar(EE), Vladimir(SK), Vladimir(UA), William(JP), Yuriy(UA), Zaur(FI)</span>
 </p>
 
-<!-- KOMMENTAAR? -->
+<!-- Reach-U keskmine täiskohale taandatud töötajate arv oli 44. Meeskonda kuulusid 2017. aastal 66 inimest (nime taga hetkel käimasolev tööaasta ja 2017. aasta auhinnad) -->
 <p class="slidetext heroes">
-<span>Tegijad:</span> <span>Aadi(1), Aire(144), Aivar(301), Aleksandr(76), Alisa(1), Ando(210), Andra(157), Andres(4), Dario(21), Elar(120), Elis(108), Gaspar(130), Gert(22), Halliki(3), Hanna(189), Helen(2), Henry(117), Indrek(128), Ingrid(49), Ivar(121), Ivo(19), Jane(34), Janno(28), Jens-Konrad(2), Jevgeni(75), Joosep(83), Juan(84), Kadri(17), Kalev(2), Kälver(30), Kaspar(6), Kasper(130), Katerina(38), Kätlin(49), Katre(81), Katrin(236), Ken(20), Kuido(17), LauriA(63), LauriK(101), Leelo(47), Leino(158), Liis(49), MadisK(1), MadisK(62), Maiga(1), Marek(4), Margus(4), Maris(21), Märit(7, Ruler, Rubik), Markus(200), Mart(287), Marta(21), MartinM(16), MartinV(28), Mati(279), Mattias(84), Meelis(1), Oliver(15), Piret(63), Rait(26, Ruler nominee), Rauni(80), Saari(2), SanderS(6, Rubik), SanderT(23, Ruler nominee), SanderV(7), Simona(4), Siret(231), SteveM(62), SteveP(130, Ruler), Sven(133), TaaviD(24), TaaviP(15), TaaviV(109), Taisi(9), TanelJ(16), TanelK(16), Teet(353), Timo(4), Toivo(148, Ruler nominee), Tõnis(62), Tõnu(3), ToomasA(37), ToomasK(15), Ülari(3), Ülo(70)</span>
+<span>Tegijad:</span> <span>Aadi(1), Aire(12), Aivar(26), Aleksandr(7), Alisa(1), Ando(18), Andra(14), Andres(1), Dario(2), Elar(10), Elis(9), Gaspar(11), Gert(2), Halliki(1), Hanna(16), Helen(1), Henry(10), Indrek(11), Ingrid(5), Ivar(11), Ivo(2), Jane(3), Janno(3), Jens-Konrad(1), Jevgeni(7), Joosep(7), Juan(7), Kadri(2), Kalev(1), Kälver(3), Kaspar(1), Kasper(11), Katerina(4), Kätlin(5), Katre(7), Katrin(20), Ken(2), Kuido(2), LauriA(6), LauriK(9), Leelo(4), Leino(14), Liis(5), MadisK(1), MadisK(6), Maiga(1), Marek(1), Margus(1), Maris(2), Märit(1, Ruler, Rubik), Markus(17), Mart(24), Marta(2), MartinM(2), MartinV(3), Mati(24), Mattias(7), Meelis(1), Oliver(2), Piret(6), Rait(3, Ruler nominee), Rauni(7), Saari(1), SanderS(1, Rubik), SanderT(2, Ruler nominee), SanderV(1), Simona(1), Siret(20), SteveM(6), SteveP(11, Ruler), Sven(12), TaaviD(2), TaaviP(2), TaaviV(10), Taisi(1), TanelJ(2), TanelK(2), Teet(30), Timo(1), Toivo(13, Ruler nominee), Tõnis(6), Tõnu(1), ToomasA(4), ToomasK(2), Ülari(1), Ülo(6)</span>
 </p>
 `;
 
@@ -57,24 +52,23 @@ Reach-U on oma pea 30 tegevusaasta jooksul arenenud kaartide tegijast geoinfosü
   const style = `
   <style>
     html, html > body { margin: 0 auto 100px auto; padding: 0}
-    body {max-width: 1024px; font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight:lighter; line-height: 20px; overflow-x: hidden}
+    body {margin: 0 auto 100px auto; max-width: 1024px; font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight:lighter; line-height: 20px; overflow-x: hidden}
     h1 {margin: 0 0 20px 0; padding: 20px; color: rgb(255, 255, 255); background: rgb(246, 89, 18) url("http://www.reach-u.com/uploads/6/1/8/4/61840011/1443454382.png") no-repeat right 10px; font-weight:lighter;}
     @media all and (max-width: 600px) {h1{padding-top: 60px; background-position: -5px 10px;}}
     p {margin: 0; padding: 10px 20px;}
     ul {list-style: none; margin: 0; padding: 0 40px 20px 40px;}
-    .salesgrowth {display: inline-block; width: 40px; color: rgb(246, 89, 18); text-align: center;}
-    h1, p, ul {opacity: 0;}
-    h1, p, ul, .salesgrowth {transition: all 0.2s linear 0s;}
+    h1, p, ul {opacity: 0; transition: all 0.2s linear 0s;}
+    a {color: inherit; text-decoration: none;} a:hover {color: rgb(246, 89, 18)}
     .slidetext {position: fixed; left: 0; right: 0; padding: 0; height: 30px; line-height: 30px; font-size: 12px; font-style: italic; white-space: nowrap; background: #eee; color: #333; border-top: solid 1px #aaa;}
     .slidetext span {display: block; position: absolute; box-sizing: border-box; top: 0; left: 0;}
     .slidetext sub {margin-right: 5px;}
-    .slidetext span:nth-child(1) {top: -1px; padding: 0 20px 0 20px; background: inherit; border: inherit; font-weight: bold; z-index: 2}
+    .slidetext span:nth-child(1) {top: -1px; padding: 0 20px; background: inherit; border: inherit; font-weight: bold; z-index: 2}
     .slidetext span:nth-child(2) {transform: translateX(3000px)}
     .slidetext.friends {bottom: 30px;}
     .slidetext.heroes {bottom: 0;}
   </style>`;
 
-  body.innerHTML = text;
+  body.innerHTML = text.replace(/(https?:\/\/[\S]+(\b|$))/gim, '<a href="$1">$1</a>');
   head.innerHTML = style;
   head.innerHTML += `<link rel="shortcut icon" href="http://pump.reach-u.com/favicon.ico"/>`;
   head.innerHTML += `<meta charset="utf-8">`;
@@ -90,6 +84,5 @@ Reach-U on oma pea 30 tegevusaasta jooksul arenenud kaartide tegijast geoinfosü
     reset();
   };
   setTimeout(() => slide($(".slidetext.friends span:nth-child(2)"), 60), 2000);
-  setTimeout(() => slide($(".slidetext.heroes span:nth-child(2)"), 60), 2000);
-  setTimeout(() => $(".salesgrowth").style.transform = "scale(1.5)", 2500);
+  setTimeout(() => slide($(".slidetext.heroes span:nth-child(2)"), 40), 2000);
 }());
